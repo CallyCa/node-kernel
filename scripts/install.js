978e2493f8ae4c25b37641a7f6fd94239424875d
callyca <blackyrite@hotmail.com>
2022-09-19 14:29:20 -0300

    Updated buhu dependency

--- scripts/install.js
+++ scripts/install.js
@@ -2,20 +2,17 @@
 
 const manager = require('download-manager')
 
-
 const PKG = require('../package.json')
 
-const LINUX_VERSION = PKG.version.split('.').filter((item)=>parseInt(item)).join('.')
-const LINUX_URL     = `${PKG.buho.url}/linux-${LINUX_VERSION}.tar.gz`
-const LINUX_SHA256  = PKG.buho.sha256
-
+const LINUX_VERSION = PKG.version.split('.').filter((item) => parseInt(item)).join('.')
+const LINUX_URL = `${PKG.buho.url}/linux-${LINUX_VERSION}.tar.gz`
+const LINUX_SHA256 = PKG.buho.sha256
 
 // Patch Linux kernel to add Unicode BMP support on `fbcon`
-const LINUX_CJKTTY_VERSION = "4.14"
-const LINUX_CJKTTY_PATCH = "https://raw.githubusercontent.com/NodeOS/cjktty-patch/master/patches/linux-"+LINUX_CJKTTY_VERSION+"-cjktty.diff"
+const LINUX_CJKTTY_VERSION = '4.14'
+const LINUX_CJKTTY_PATCH = 'https://raw.githubusercontent.com/NodeOS/cjktty-patch/master/patches/linux-' + LINUX_CJKTTY_VERSION + '-cjktty.diff'
 
-
-var download =
+const download =
 {
   name: 'linux',
   url: LINUX_URL,
@@ -37,14 +34,10 @@ var download =
   ]
 }
 
-if(process.platform === 'darwin')
-  download.patch = 'patches/linux-darwin.diff'
+if (process.platform === 'darwin') { download.patch = 'patches/linux-darwin.diff' }
 
-
-manager(download, function(error)
-{
-  if(error)
-  {
+manager(download, function (error) {
+  if (error) {
     console.trace(error)
     process.exit(1)
   }
